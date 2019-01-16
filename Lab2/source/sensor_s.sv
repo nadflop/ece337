031411a66cd284991ff6e29e54c8e620ce8af174
Nur Nadhira Aqilah Binti Mohd Shah <mohdshah@purdue.edu>
2019-01-16 12:10:00 -0500

    finish lab 2

--- Lab2/source/sensor_s.sv
+++ Lab2/source/sensor_s.sv
@@ -9,10 +9,10 @@
 module sensor_s(
 
 	input wire [3:0] sensors,
-	output reg error
+	output wire error
 );
-	reg int_or1;
-	reg int_and1;
+	wire int_or1;
+	wire int_and1;
 
 	OR2X1 A1 (.Y(int_or1), .A(sensors[3]), .B(sensors[2]));
 	AND2X1 A2 (.Y(int_and1), .A(sensors[1]), .B(int_or1));
